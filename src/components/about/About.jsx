78a1d3201a3f79c077de80b48fa7c8ab0fc3f471
Abishethvarman V <abishethvarman@gmail.com>
2022-08-01 01:10:52 +0530

    final

---
+++ src/components/about/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./about.css"
+
+export const About = () => {
+  return (
+    <div className='a'>
+        <div className="a-left">Abishek</div>
+            <div className="a-card"></div>
+            <div className="a-card"></div>
+                <img src="" alt="" className="a-img" />
+        <div className="a-right"></div>
+    </div>
+  )
+}
+
+
